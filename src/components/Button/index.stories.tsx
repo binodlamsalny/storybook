4a5c1db511697b45dc03a313052101895bfc7768
binodlamsalny <binodlamsalny@gmail.com>
2024-11-18 13:21:53 +0545

    first component is complete

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -14,8 +14,32 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
     args: {
-        children: "Click me",
-    }
-}
+        variant: "solid",
+        children: "Button",
+    },
+};
+
+export const Outline: Story = {
+    args: {
+        variant: "outline",
+        children: "Button",
+    },
+};
+
+export const Ghost: Story = {
+    args: {
+        variant: "ghost",
+        children: "Button",
+    },
+};
+
+// export const Default: Story = {
+//     args: {
+//         children: "Click me",
+//         variant: "solid",
+//         size: "md",
+//         colorscheme: "primary",
+//     }
+// }
